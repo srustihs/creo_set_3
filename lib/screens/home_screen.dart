16d5b0011b4227f4aa7f1b0f54138bae16ee3e40
srustihs <124963723+srustihs@users.noreply.github.com>
2023-05-01 11:51:38 +0530

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,153 @@
 import 'package:flutter/material.dart';
-import '../widgets/bottom_nav_bar.dart';
+import 'package:flutter_login/models/article_model.dart';
+import 'package:flutter_login/screens/screens.dart';
+import 'package:flutter_login/widgets/custom_tag.dart';
 
+import '../widgets/bottom_nav_bar.dart';
+import '../widgets/image_container.dart';
+//
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({Key? key, required Article article}) : super(key: key);
+//
+//   static const routeName = '/';
+//   @override
+//   Widget build(BuildContext context) {
+//     Article article = Article.articles[0];
+//
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.transparent,
+//         elevation: 0,
+//         leading: IconButton(
+//           onPressed: () {},
+//           icon: const Icon(
+//             Icons.menu,
+//             color: Colors.white,
+//           ),
+//         ),
+//       ),
+//       bottomNavigationBar: const BottomNavBar(index: 0),
+//       extendBodyBehindAppBar: true,
+//       body: ListView(padding: EdgeInsets.zero, children: [
+//         _NewsOfTheDay(article: article),
+//         _BreakingNews(articles: Article.articles),
+//       ]),
+//     );
+//   }
+// }
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({
+  static const routeName = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    Article article = Article.articles[0];
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      bottomNavigationBar: const BottomNavBar(index: 0),
+      extendBodyBehindAppBar: true,
+      body: ListView(padding: EdgeInsets.zero, children: [
+        _NewsOfTheDay(article: article),
+        _BreakingNews(articles: Article.articles),
+      ]),
+    );
+  }
+}
+
+class _BreakingNews extends StatelessWidget {
+  const _BreakingNews({
+    Key? key,
+    required this.articles,
+  }) : super(key: key);
+
+  final List<Article> articles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Breaking News',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              Text('More', style: Theme.of(context).textTheme.bodyLarge),
+            ],
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 250,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: articles.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  margin: const EdgeInsets.only(right: 10),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        ArticleScreen.routeName,
+                        arguments: articles[index],
+                      );
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ImageContainer(
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          imageUrl: articles[index].imageUrl,
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          articles[index].title,
+                          maxLines: 2,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge!
+                              .copyWith(
+                              fontWeight: FontWeight.bold, height: 1.5),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                            '${DateTime.now().difference(articles[index].createdAt).inHours} hours ago',
+                            style: Theme.of(context).textTheme.bodySmall),
+                        const SizedBox(height: 5),
+                        Text('by ${articles[index].author}',
+                            style: Theme.of(context).textTheme.bodySmall),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _NewsOfTheDay extends StatelessWidget {
+  const _NewsOfTheDay({
     Key? key,
     required this.article,
   }) : super(key: key);
@@ -11,113 +156,53 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return ImageContainer(
+      height: MediaQuery.of(context).size.height * 0.45,
+      width: double.infinity,
+      padding: const EdgeInsets.all(20.0),
+      imageUrl: article.imageUrl,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomTag(
+            backgroundColor: Colors.grey.withAlpha(150),
             children: [
               Text(
-                'News of the day',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6!
-                    .copyWith(fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              InkWell(
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    // ArticleScreen.routeName,
-                    arguments: article,
-                  );
-                },
-                child: Stack(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: ImageContainer(
-                        height: 200,
-                        imageUrl: article.imageUrl,
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 10,
-                      left: 10,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 5,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.8),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Text(
-                          article.category.toUpperCase(),
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 10,
-                      right: 10,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 5,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.8),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.access_time,
-                              size: 16,
-                              color: Colors.black,
-                            ),
-                            const SizedBox(width: 5),
-                            Text(
-                              '${DateTime.now().difference(article.createdAt).inHours} hours ago',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+                'News of the Day',
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Colors.white,
                 ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                article.title,
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6!
-                    .copyWith(fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 5),
-              Text(
-                article.description,
-                style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                  height: 1.5,
-                ),
-              ),
-              const SizedBox(height: 5),
-              Text(
-                'by ${article.author}',
-                style: Theme.of(context).textTheme.bodyText2,
               ),
             ],
-        ),
-        );
-    }
+          ),
+          const SizedBox(height: 10),
+          Text(
+            article.title,
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                fontWeight: FontWeight.bold, height: 1.25, color: Colors.white),
+          ),
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(padding: EdgeInsets.zero),
+            child: Row(
+              children: [
+                Text(
+                  'Learn More',
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                const Icon(
+                  Icons.arrow_right_alt,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
